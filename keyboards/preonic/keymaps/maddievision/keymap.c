1769504160e3a7ca6ca5ad91d3e037d6047bb7c6
maddie lim ✨ <maddie@maddie.vision>
2021-04-30 03:41:58 -0700

    :crocodile: fix some things. swap extra and ctrl key. update startup jingle!

--- keyboards/preonic/keymaps/maddievision/keymap.c
+++ keyboards/preonic/keymaps/maddievision/keymap.c
@@ -9,7 +9,7 @@
 enum preonic_layers {
   _BASE_QWERTY,
   _NUM_ROW_EXT,
-  _MEDIA_BRACKETS
+  _MEDIA_BRACKETS,
   _EXTRA,
 
   _LOWER,
@@ -18,8 +18,8 @@ enum preonic_layers {
 };
 
 // transparent and nop
-#define KTR K_TRNS
-#define KNO K_NO
+#define KTR KC_TRNS
+#define KNO KC_NO
 
 // momentary
 #define MO_LWR MO(_LOWER)
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    LT_MDIA,
         KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-        MO_EXT,  KC_LCTL, KC_LALT, KC_LGUI, MO_LWR,  KC_SPC,  KC_SPC,  MO_RSE,  KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
+        KC_LCTL, MO_EXT,  KC_LALT, KC_LGUI, MO_LWR,  KC_SPC,  KC_SPC,  MO_RSE,  KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
     ),
 
 	[_MEDIA_BRACKETS] = LAYOUT_ortho_5x12(
